55a900c09ad6a1b966e4379c57e71b75f13f606f
craytep <datekin@mail.ru>
2021-09-08 19:12:53 +0300

    Работающий код, ура, вывод в матлабе с графиком, а также поиск максимума на графике и вывод значений из области максимума

--- serialUScheck/serialv1.m
+++ serialUScheck/serialv1.m
@@ -2,23 +2,30 @@ clc
 clear all
 close all
 port = serialportlist;
-s1 = serialport(port(1), 115200,"Timeout",10);
+A = []
+s1 = serialport(port(2), 115200,"Timeout",10);
 s1.Timeout = 2;
 flush(s1);
-val='';
-A = []
-for c = 1:1e5
+
+val=str2num(convertStringsToChars(readline(s1)));
+
+while val~= 5000
+val=str2num(convertStringsToChars(readline(s1)));
+end
+A = [A, val]
+val=str2num(convertStringsToChars(readline(s1)));
+while val~= 5000
 
 
-
-    val=str2num(convertStringsToChars(readline(s1)))
-    A=[A,val];
+   A=[A,val];
+    val=str2num(convertStringsToChars(readline(s1)));
+     
     
 
 end
 %figure
 
-%plot(A)
+plot(A)
 %pause(0.1)
 
 clear s1
