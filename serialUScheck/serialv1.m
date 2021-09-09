9c7841b12506cb0fea8fb702ddb63631cec24644
craytep <datekin@mail.ru>
2021-09-09 18:13:53 +0300

    номер парсер

--- serialUScheck/serialv1.m
+++ serialUScheck/serialv1.m
@@ -4,7 +4,7 @@ close all
 port = serialportlist;
 A = []
 s1 = serialport(port(2), 115200,"Timeout",10);
-s1.Timeout = 2;
+s1.Timeout = 20000;
 flush(s1);
 
 val=str2num(convertStringsToChars(readline(s1)));
@@ -12,23 +12,17 @@ val=str2num(convertStringsToChars(readline(s1)));
 while val~= 5000
 val=str2num(convertStringsToChars(readline(s1)));
 end
-for c =1:10
-val=str2num(convertStringsToChars(readline(s1)));
-while val~= 5000
-
-
-   A=[A,val];
-    val=str2num(convertStringsToChars(readline(s1)));
-     
-    
-
+A = [];
+for c =1:40
+while all(A ~= 5000)
+A=[A; str2num(convertStringsToChars(read(s1,6e4,"string")))];
 end
-
 figure
-plot(A)
+A(length(A))=[];
+plot(A(1:find(A == 5000)-1))
+
 pause(0.1)
-clear A
-A = []
+A = A(find(A == 5000)+1:end)
 end
 
 
